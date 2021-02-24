f4b5ba6843ba7e1e11f38828b8bc8416d90bc54e
Tomás Generelo <tgenerelo@gmail.com>
2021-02-24 12:26:45 +0100

    Auto stash before merge of "main" and "origin/main"

--- src/agentesTIA/Main.java
+++ src/agentesTIA/Main.java
@@ -1,9 +1,44 @@
 package agentesTIA;
 
+import menus.Menus;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		int userInput = 0;
+
+		do {
+			userInput = Menus.mostrarMenu();
+
+			switch (userInput) {
+			case 1:
+
+				break;
+
+			case 2:
+
+				break;
+			case 3:
+
+				break;
+			case 4:
+
+				break;
+			case 5:
+
+				break;
+			case 6:
+
+				break;
+			case 7:
+
+				break;
+			case 8:
+
+				break;
+			}
+		} while (userInput != 8);
 
 	}
 
