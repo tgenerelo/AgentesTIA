da93d39598d8c0da7f0c05108a671c9a6f776268
silvia <sielma712@gmail.com>
2021-02-26 13:46:49 +0100

    mejorado busqueda salario y reorganizar metodos

--- src/menus/Metodos.java
+++ src/menus/Metodos.java
@@ -7,6 +7,9 @@ import java.io.PrintWriter;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import IODatos.IODatos;
+import agentesTIA.A007;
+import agentesTIA.AEspionaje;
 import agentesTIA.ASuperintendente;
 import agentesTIA.Agente;
 
@@ -24,12 +27,23 @@ public class Metodos {
 			
 			System.out.println("Introduce el salario minimo");
 			Scanner leer=new Scanner(System.in);
-			int cantidad =leer.nextInt();
+			float cantidad =leer.nextInt();
+			int cont=0;
+		
+			
+			
+			
+			
 			
 			for(int i=0; i<vAgente.length;i++) {
 				if((vAgente[i]!=null) && (cantidad<=vAgente[i].getSalario())){
 					System.out.println(vAgente[i]);
+					cont++;
 				}
+			}
+			
+			if (cont==0) {
+				System.out.println("no se han encontrado resultados");
 			}
 		}
 	
@@ -69,59 +83,254 @@ public class Metodos {
 				}
 		}	
 	
+
+	private static int encontrarHuecoVector(Agente[] vAgentes) {
+
+		for (int i = 0; i < vAgentes.length; i++) {
+			if (vAgentes[i] == null) {
+				return i;
+			}
+		}
+
+		return -1;
+	}
 	
 	
 	
-	public static Agente nuevoAgente() {
+	public static Agente[] nuevoAgente(Agente[] vAgentes, String ruta) {
 
 		Scanner leerInput = new Scanner(System.in);
-		int userInput = 0;
+		int userInputInt = 0, pos = 0;
 		boolean error = false;
+		String nombreAgente = "", dirAgente = "";
+		int edadAgente = 0;
+		float salarioAgente = 0f;
+		String vTiposAgente[] = { "Agente de espionaje", "Agente 007", "Superintendente" };
 
 		System.out.println("Especifica el tipo de agente que quieres dar de alta:");
-		System.out.println("1. Agente de espionaje");
-		System.out.println("2. Agente 007");
-		System.out.println("3. Superintendente");
-		
+
+		for (int i = 0; i < vTiposAgente.length; i++) {
+			System.out.println((i + 1) + ". " + vTiposAgente[i]);
+		}
 
 		do {
 			error = false;
 
 			System.out.print("\nTipo de agente: > ");
-			
+
 			try {
-				userInput = leerInput.nextInt();
+				userInputInt = leerInput.nextInt();
 			} catch (InputMismatchException e) {
 				error = true;
-				leerInput=new Scanner(System.in);
+				leerInput = new Scanner(System.in);
 				System.out.println("Opción no válida. Inténtalo de nuevo.\n");
 			}
-			
-			if (userInput <1 || userInput >3) {
+
+			if (userInputInt < 1 || userInputInt > vTiposAgente.length) {
 				error = true;
 			} else {
 				error = false;
 			}
 
 		} while (error == true);
-		
-		switch (userInput) {
+
+		System.out.println();
+
+		Menus.pintarSubmenu(vTiposAgente[userInputInt - 1]);
+
+		switch (userInputInt) {
+
 		case 1:
-			// Agente de espionaje
+
+			leerInput = new Scanner(System.in);
+			System.out.print(" Introduce el nombre del nuevo agente: > ");
+			nombreAgente = leerInput.nextLine();
+
+			do {
+				leerInput = new Scanner(System.in);
+				System.out.print(" Introduce la edad del nuevo agente: > ");
+				error = false;
+				try {
+					edadAgente = leerInput.nextInt();
+				} catch (InputMismatchException e) {
+					System.out.println("Edad no válida. Inténtalo de nuevo.");
+					error = true;
+				} catch (Exception e) {
+					System.out.println("Se ha producido un error desconocido (E1). Inténtalo de nuevo.");
+					error = true;
+				}
+			} while (error == true);
+
+			leerInput = new Scanner(System.in);
+			System.out.print(" Introduce la dirección del nuevo agente: > ");
+			dirAgente = leerInput.nextLine();
+
+			do {
+				leerInput = new Scanner(System.in);
+				System.out.print(" Introduce el salario del nuevo agente: > ");
+				error = false;
+				try {
+					salarioAgente = leerInput.nextFloat();
+				} catch (InputMismatchException e) {
+					System.out.println("Salario no válido. Inténtalo de nuevo.");
+					error = true;
+				} catch (Exception e) {
+					System.out.println("Se ha producido un error desconocido (E2). Inténtalo de nuevo.");
+					error = true;
+				}
+			} while (error == true);
+
+			pos = encontrarHuecoVector(vAgentes);
+
+			if (pos != -1) {
+				vAgentes[pos] = new AEspionaje(nombreAgente, edadAgente, dirAgente, salarioAgente);
+			} else {
+				System.out.println("Error al dar de alta el agente. No hay huecos libres.");
+			}
+			
 			break;
 
 		case 2:
-			// Agente 007
+
+			int muertesAgente = 0;
+
+			leerInput = new Scanner(System.in);
+			System.out.print(" Introduce el nombre del nuevo agente: > ");
+			nombreAgente = leerInput.nextLine();
+
+			do {
+				leerInput = new Scanner(System.in);
+				System.out.print(" Introduce la edad del nuevo agente: > ");
+				error = false;
+				try {
+					edadAgente = leerInput.nextInt();
+				} catch (InputMismatchException e) {
+					System.out.println("Edad no válida. Inténtalo de nuevo.");
+					error = true;
+				} catch (Exception e) {
+					System.out.println("Se ha producido un error desconocido (E11). Inténtalo de nuevo.");
+					error = true;
+				}
+			} while (error == true);
+
+			leerInput = new Scanner(System.in);
+			System.out.print(" Introduce la dirección del nuevo agente: > ");
+			dirAgente = leerInput.nextLine();
+
+			do {
+				leerInput = new Scanner(System.in);
+				System.out.print(" Introduce el salario del nuevo agente: > ");
+				error = false;
+				try {
+					salarioAgente = leerInput.nextFloat();
+				} catch (InputMismatchException e) {
+					System.out.println("Salario no válido. Inténtalo de nuevo.");
+					error = true;
+				} catch (Exception e) {
+					System.out.println("Se ha producido un error desconocido (E12). Inténtalo de nuevo.");
+					error = true;
+				}
+			} while (error == true);
+
+			do {
+				leerInput = new Scanner(System.in);
+				System.out.print(" Introduce el número de bajas del nuevo agente: > ");
+				error = false;
+				try {
+					muertesAgente = leerInput.nextInt();
+				} catch (InputMismatchException e) {
+					System.out.println("Número no válido. Inténtalo de nuevo.");
+					error = true;
+				} catch (Exception e) {
+					System.out.println("Se ha producido un error desconocido (E13). Inténtalo de nuevo.");
+					error = true;
+				}
+			} while (error == true);
+
+			pos = encontrarHuecoVector(vAgentes);
+
+			if (pos != -1) {
+				vAgentes[pos] = new A007(nombreAgente, edadAgente, dirAgente, salarioAgente, muertesAgente);
+			} else {
+				System.out.println("Error al dar de alta el agente. No hay huecos libres.");
+			}
+
+			break;
+
+		case 3:
+			int anosMandato = 0;
+
+			leerInput = new Scanner(System.in);
+			System.out.print(" Introduce el nombre del nuevo agente: > ");
+			nombreAgente = leerInput.nextLine();
+
+			do {
+				leerInput = new Scanner(System.in);
+				System.out.print(" Introduce la edad del nuevo agente: > ");
+				error = false;
+				try {
+					edadAgente = leerInput.nextInt();
+				} catch (InputMismatchException e) {
+					System.out.println("Edad no válida. Inténtalo de nuevo.");
+					error = true;
+				} catch (Exception e) {
+					System.out.println("Se ha producido un error desconocido (E11). Inténtalo de nuevo.");
+					error = true;
+				}
+			} while (error == true);
+
+			leerInput = new Scanner(System.in);
+			System.out.print(" Introduce la dirección del nuevo agente: > ");
+			dirAgente = leerInput.nextLine();
+
+			do {
+				leerInput = new Scanner(System.in);
+				System.out.print(" Introduce el salario del nuevo agente: > ");
+				error = false;
+				try {
+					salarioAgente = leerInput.nextFloat();
+				} catch (InputMismatchException e) {
+					System.out.println("Salario no válido. Inténtalo de nuevo.");
+					error = true;
+				} catch (Exception e) {
+					System.out.println("Se ha producido un error desconocido (E12). Inténtalo de nuevo.");
+					error = true;
+				}
+			} while (error == true);
+
+			do {
+				leerInput = new Scanner(System.in);
+				System.out.print(" Introduce los años de mandato del nuevo agente: > ");
+				error = false;
+				try {
+					anosMandato = leerInput.nextInt();
+				} catch (InputMismatchException e) {
+					System.out.println("Número no válido. Inténtalo de nuevo.");
+					error = true;
+				} catch (Exception e) {
+					System.out.println("Se ha producido un error desconocido (E13). Inténtalo de nuevo.");
+					error = true;
+				}
+			} while (error == true);
+
+			pos = encontrarHuecoVector(vAgentes);
+
+			if (pos != -1) {
+				vAgentes[pos] = new ASuperintendente(nombreAgente, edadAgente, dirAgente, salarioAgente, anosMandato);
+			} else {
+				System.out.println("Error al dar de alta el agente. No hay huecos libres.");
+			}
+
 			break;
 			
-		case 3:
-			// Superintendente
-			break;
 		}
-		
-		return new ASuperintendente(null, userInput, null, userInput, userInput); // 
-		
+
+		System.out.println();
+		IODatos.guardarAgentes(ruta, vAgentes);
+		return vAgentes;
+
 	}
+	
 
 
 }
